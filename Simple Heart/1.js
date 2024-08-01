4d29bf0d96cde49edb2c1638b040e3d902345bfd
Truong666 <bangvesin@gmail.com>
2024-08-01 20:13:09 +0700

    Delete Simple Heart directory

--- Simple Heart/1.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("click", function() {
-    document.querySelector(".heart").style.fontSize = "150px";
-    setTimeout(function() {
-      document.querySelector(".heart").style.fontSize = "100px";
-    }, 2000);
-  });
